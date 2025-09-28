ed1ec9ce4ad4f7c030a04d03d611746c68d605fa
Kasilnur <newlega2021@gmail.com>
2025-09-28 17:51:48 +0500

    create Appsettings

--- RentHub/RentHub.Core/obj/Debug/net8.0/RentHub.Core.AssemblyInfo.cs
+++ RentHub/RentHub.Core/obj/Debug/net8.0/RentHub.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentHub.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da72e11d5ff97729e67b74970f2ce1cc166b73a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ffaa301086c49f884adda318ce5b0edbf46829")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentHub.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentHub.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
