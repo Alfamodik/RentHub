91dd326fd4613927c78116ef2561412f02e5ac48
DESKTOP-HI59H2R\Alfamod <Salnikovdmitry.2006@gmail.com>
2025-10-23 20:51:02 +0500

    Added sutochno parser

---
+++ RentHub/RentHub.API/Services/SutochnoParser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Playwright;
+
+namespace RentHub.API.Services
+{
+    public static class SutochnoParser
+    {
+        public async static Task<string> Get()
+        {
+            var url = "https://sutochno.ru/front/searchapp/detail/1966081";
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = true
+            });
+
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync(url, new PageGotoOptions
+            {
+                WaitUntil = WaitUntilState.NetworkIdle
+            });
+
+            var html = await page.ContentAsync();
+            return html;
+        }
+    }
+}
