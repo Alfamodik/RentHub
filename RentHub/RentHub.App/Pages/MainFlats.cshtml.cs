a77b646d08904578cb14f66414e29a4e8fa40473
DESKTOP-HI59H2R\Alfamod <Salnikovdmitry.2006@gmail.com>
2025-11-03 14:28:08 +0500

    Adde flats page

--- RentHub/RentHub.App/Pages/MainFlats.cshtml.cs
+++ RentHub/RentHub.App/Pages/MainFlats.cshtml.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RentHub.Core.Model;
 
 namespace RentHub.App.Pages
 {
     public class MainFlatsModel : PageModel
     {
+        public List<Flat> Flats { get; set; } = new();
         public void OnGet()
         {
+
         }
+
+
     }
 }
