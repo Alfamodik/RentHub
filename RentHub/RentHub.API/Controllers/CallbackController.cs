987603cd085e562ed497043ef63e2e79f830436b
DESKTOP-HI59H2R\Alfamod <Salnikovdmitry.2006@gmail.com>
2025-11-14 00:28:15 +0500

    Add autrorize to callback

--- RentHub/RentHub.API/Controllers/CallbackController.cs
+++ RentHub/RentHub.API/Controllers/CallbackController.cs
@@ -1,6 +1,9 @@
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RentHub.API.ResponceModels.Avito;
 using RentHub.Core.Model;
+using System.Security.Claims;
 using System.Text.Json;
 
 namespace RentHub.API.Controllers
@@ -9,11 +12,23 @@ namespace RentHub.API.Controllers
     [ApiController]
     public class CallbackController : ControllerBase
     {
+        [Authorize]
         [HttpGet]
         public async Task<IActionResult> Get([FromQuery] string code)
         {
             if (string.IsNullOrEmpty(code))
                 return BadRequest("Код отсутствует.");
+
+            string? claimedUserid = User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            if (!int.TryParse(claimedUserid, out int userId))
+                return Unauthorized();
+
+            RentHubContext context = new();
+            User? user = context.Users.FirstOrDefault(user => user.UserId == userId);
+
+            if (user == null)
+                return Unauthorized();
 
             HttpClient _httpClient = new()
             {
@@ -26,7 +41,7 @@ namespace RentHub.API.Controllers
                 { "client_id", "hqQSMZCzT_szv7cre5vG" },
                 { "client_secret", "e8oWwQ9S7nsbmuBsr_MuALR8nqcRxtHtyDBrt-YN" },
                 { "code", code },
-                { "redirect_uri", "http://94.183.186.221:5000/Callback" }
+                { "redirect_uri", "http://94.183.186.221:5168/Callback" }
             };
 
             HttpResponseMessage response = await _httpClient.PostAsync("token", new FormUrlEncodedContent(data));
@@ -42,18 +57,16 @@ namespace RentHub.API.Controllers
             if (accessTokenResponse == null)
                 return BadRequest("Ошибка десериализации ответа от Avito");
 
-            RentHubContext context = new();
-            User? user = context.Users.FirstOrDefault(user => user.UserId == 6);
+            if (accessTokenResponse.AccessToken != null && accessTokenResponse.RefreshToken != null)
+            {
+                user.AvitoAccessToken = accessTokenResponse.AccessToken;
+                user.AvitoRefreshToken = accessTokenResponse.RefreshToken;
+                user.TokenExpiresAt = DateOnly.FromDateTime(DateTime.Now);
+                context.SaveChanges();
+                return Ok(accessTokenResponse);
+            }
 
-            if (user == null)
-                return NotFound();
-
-            user.AvitoAccessToken = accessTokenResponse.AccessToken;
-            user.AvitoRefreshToken = accessTokenResponse.RefreshToken;
-            user.TokenExpiresAt = DateOnly.FromDateTime(DateTime.Now);
-
-            context.SaveChanges();
-            return NoContent();
+            return Ok(new { error = "Не удалось получить доступ" });
         }
     }
 }
